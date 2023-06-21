ace2c2cfea48352c0c3a1b2d00ec6cb324936e9f
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-06-21 13:56:46 +0300

    güncelleme
    güncelleme

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const twilio= require('twilio');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
